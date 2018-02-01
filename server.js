15e35095dea5e2bedfc99c1d7aff3f74f148afd9
rahmona-henry <rahmona.henry@icloud.com>
2018-02-01 00:20:30 +0000

    basic file structure

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Clontarf')
+app.use(express.static('client'))
+
+app.get('/', (req, res) => res.send('Hello Clontarf'))
 app.listen(3000, () => console.log('Catching a ride on port 3000'))
